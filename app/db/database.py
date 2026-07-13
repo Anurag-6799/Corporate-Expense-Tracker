f5be2f65e5eed31a1a18b2d5ea8e77249f0e111a
Anurag-6799 <singhanurag6799@gmail.com>
2026-07-13 21:39:28 +0530

    feat: initialize SQLAlchemy engine, session factory, and base model with dependency injection setup

---
+++ app/db/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from app.core.config import settings
+
+# The Engine: The actual communication pipeline to PostgreSQL
+engine = create_engine(settings.DATABASE_URL)
+
+# The Session Factory: Generates temporary database sessions for our API requests
+# autocommit=False ensures transactions aren't saved until we explicitly call db.commit()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# The Base Class: All our future database models will inherit from this
+class Base(DeclarativeBase):
+    pass
+
+# The Dependency (The Yield Pattern)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()  # This guarantees the connection returns to the pool
