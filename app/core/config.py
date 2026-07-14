ab41b6bd5f3642e8adf3300f11d58be076396c87
Anurag-6799 <singhanurag6799@gmail.com>
2026-07-14 09:03:24 +0530

    refactor: configure SettingsConfigDict for case-insensitive environment variable loading

--- app/core/config.py
+++ app/core/config.py
@@ -4,13 +4,10 @@ class Settings(BaseSettings):
     PROJECT_NAME: str
     SECRET_KEY: str
     DATABASE_URL: str
-    
-    # Security Configurations (We will use these later for JWTs)
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
-    # This tells Pydantic to look for a file named .env in the root directory
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 
+
     model_config = SettingsConfigDict(env_file=".env")
 
-# We instantiate this exactly once. All other files will import this 'settings' object.
-settings = Settings()
+settings = Settings()  
